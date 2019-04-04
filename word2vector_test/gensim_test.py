5fd9f2f859e40118d717fd1ef538d1cc50ae1384
MoonKuma <1244074267@qq.com>
2019-04-04 17:03:17 +0800

    process pipeline

--- word2vector_test/gensim_test.py
+++ word2vector_test/gensim_test.py
@@ -5,6 +5,7 @@
 # @Date  : 2019/3/27
 # @Desc  : test the simplest usage of genism
 
+import gensim
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 import multiprocessing
@@ -13,7 +14,7 @@ import os
 
 default_target_file = 'data/formal_data/test1/zh.moegirl_1W-data-opencc-jieba.txt'
 default_model = 'data/formal_data/test1/zh.moegirl_1W-data-opencc-jieba-model1.txt'
-
+model2_file = 'data/formal_data/test2/zh.moegirl_200000-data-opencc-jieba-model1.txt'
 
 def genism_test(target_file=default_target_file):
     # the original version on training w2v
@@ -23,15 +24,21 @@ def genism_test(target_file=default_target_file):
     model = Word2Vec(LineSentence(target_file), size=200, window=5, min_count=5, workers=multiprocessing.cpu_count())
     model.save(result_file_1)
     model.wv.save_word2vec_format(result_file_2, binary=False)
+    gensim.models.KeyedVectors.load_word2vec_format
 
 
 # test reading
-def test_model_result(model_file=default_model, test_words=u"娘"):
+current_model = None
+def test_model_result(model_file=default_model, test_words=u"喜欢", reset=False):
     # testing result of model
-    model = Word2Vec.load(model_file)
-    for element in model.wv.most_similar(test_words):
+    global current_model
+    if current_model is None or reset:
+        current_model = Word2Vec.load(model_file)
+    for element in current_model.wv.most_similar(positive=test_words):
         print(element[0],element[1])
-
+    current_model.wv.get_keras_embedding()
+#
+test_model_result(model_file=model2_file,test_words=u"可爱")
 
 # add on training
 def genism_add_on(model_file=default_model, target_file=default_target_file):
@@ -46,4 +53,5 @@ def genism_add_on(model_file=default_model, target_file=default_target_file):
     model.train(LineSentence(target_file), total_examples=examples, epochs=model.iter)
     model.save(result_file_1)
     model.wv.save_word2vec_format(result_file_2, binary=False)
+
 
